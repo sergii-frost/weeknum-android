c55a2e45560bc68920275e85f45bd42d4a850436
Sergii Nezdolii <sergii@frost.se>
2017-12-07 00:19:01 +0100

    [UI] adding buttons on click listeners

--- Weeknum/app/src/main/java/se/frost/weeknum/MainActivity.kt
+++ Weeknum/app/src/main/java/se/frost/weeknum/MainActivity.kt
@@ -2,11 +2,24 @@ package se.frost.weeknum
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBindings()
+    }
+
+    private fun setupBindings() {
+        previosWeekButton.setOnClickListener { toast("Previous") }
+        todayButton.setOnClickListener { toast("Today") }
+        nextWeekButton.setOnClickListener { toast("Next") }
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
